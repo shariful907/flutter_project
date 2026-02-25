57564b1824cb7f78a26b7476c0a007dd87e35fbe
Md Shariful Islam <sharif.beh.it@gmail.com>
2026-02-25 14:45:35 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tasbih_counter/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
+
+
 
   // This widget is the root of your application.
   @override
@@ -13,104 +17,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Update & modify Counter App'),
+      home: home_page(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  void _decrementCounter(){
-    setState(() {
-      if(_counter > 0){
-        _counter--;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Counter App:',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [SizedBox(height: 70),
-                ElevatedButton(onPressed: _incrementCounter,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      fixedSize: Size(120, 40)
-                    ),
-                    child: Text("Increment")),
-
-                SizedBox(width: 20),
-                ElevatedButton(onPressed: _decrementCounter,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.redAccent,
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                        fixedSize: Size(120, 40),
-                    ),
-                    child: Text("Decrement")),
-
-                SizedBox(width: 20),
-                ElevatedButton(onPressed: (){setState(() {
-                  _counter = 0;
-                });},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueAccent,
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                        fixedSize: Size(120, 40)
-                    ),
-                    child: Text("Reset"))
-              ],
-            )
-          ],
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
